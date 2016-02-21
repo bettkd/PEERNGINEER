6ceea68e1985dc7b696e9b928e0110418e5c465b
Dominic Bett <dominicbett@hotmail.com>
2016-02-21 15:37:05 -0500

    build profile

--- routes/profile.js
+++ routes/profile.js
@@ -17,6 +17,15 @@ router.get('/', function(req, res) {
 		viewObj.email = email
 		viewObj.username = email.substring(0, email.lastIndexOf("@"));
 		viewObj.profileImageURL = authData.password.profileImageURL;
+		viewObj.firstname = "FNAME";
+		viewObj.lastname = "LNAME";
+		viewObj.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sem tortor, aliquam viverra commodo nec, semper at leo. Phasellus fringilla porttitor arcu, a sollicitudin enim euismod ac. Nulla elementum mi vel elit consectetur, ac ultrices quam hendrerit. Nulla mattis mollis ex ut eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing.";
+		viewObj.phone = "+1 (803) 000-0000";
+		viewObj.linkedinID = "userID";
+		viewObj.facebookID = "userID";
+		viewObj.githubID = "userID";
+		viewObj.major = "Computer Science";
+		viewObj.classification = "Junior";
 
 		console.log("Image url:", authData.password.profileImageURL);
 		res.render('profile', viewObj)
